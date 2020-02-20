0979489c2fe0fd3168adf85a9f607ed58a052a48
Martin Mosegaard Amdisen <martin@chainalysis.com>
2020-02-20 15:06:06 +0100

    Minor doc edits
    Fixed a typo and changed "GitHub repos" to just "Git repos" because
    `atlantis_allowed_repo_names` is also used for GitLab.

--- variables.tf
+++ variables.tf
@@ -137,7 +137,7 @@ variable "atlantis_bitbucket_user_token_ssm_parameter_name" {
 }
 
 variable "ssm_kms_key_arn" {
-  description = "ARN of KMS key to use for entryption and decryption of SSM Parameters. Required only if your key uses a custom KMS key and not the default key"
+  description = "ARN of KMS key to use for encryption and decryption of SSM Parameters. Required only if your key uses a custom KMS key and not the default key"
   type        = string
   default     = ""
 }
@@ -222,7 +222,7 @@ variable "atlantis_repo_whitelist" {
 }
 
 variable "atlantis_allowed_repo_names" {
-  description = "Github repositories where webhook should be created"
+  description = "Git repositories where webhook should be created"
   type        = list(string)
   default     = []
 }
