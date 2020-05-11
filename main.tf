6d7c63cc19568cbd342ce599dde110edeed43778
Anton Babenko <anton@antonbabenko.com>
2020-05-11 21:57:51 +0200

    Fixed alb output

--- main.tf
+++ main.tf
@@ -298,7 +298,7 @@ resource "aws_route53_record" "atlantis" {
 
   alias {
     name                   = module.alb.this_lb_dns_name
-    zone_id                = module.alb.load_balancer_zone_id
+    zone_id                = module.alb.this_lb_zone_id
     evaluate_target_health = true
   }
 }
