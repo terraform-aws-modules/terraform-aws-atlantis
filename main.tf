c74fa0c234a76719865df2affa03594c4b527cfd
Paris Morali <paris.morali@freeagent.com>
2020-12-10 14:34:17 +0000

    Apply PR feedback to improve list concat

--- main.tf
+++ main.tf
@@ -373,7 +373,7 @@ data "aws_iam_policy_document" "ecs_tasks" {
 
     principals {
       type        = "Service"
-      identifiers = concat(["ecs-tasks.amazonaws.com"], var.trusted_principals)
+      identifiers = compact(distinct(concat(["ecs-tasks.amazonaws.com"], var.trusted_principals)))
     }
   }
 }
