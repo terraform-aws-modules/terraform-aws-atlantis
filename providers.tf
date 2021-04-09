2c7749e4902c9c34058cad458df985506ff6d09b
Gabrielle Singh Cadieux <gabrielle.cadieux@tophatmonocle.com>
2021-04-09 17:44:29 -0400

    chore: add remote backend

--- providers.tf
+++ providers.tf
@@ -1,4 +1,14 @@
 provider "aws" {
-  profile = "tophat-dev"
   region  = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket               = "tophat-terraform-state"
+    key                  = "state.tfstate"
+    workspace_key_prefix = "atlantis-service"
+    region               = "us-east-1"
+    dynamodb_table       = "tophat-terraform-state-locks"
+    acl                  = "bucket-owner-full-control"
+  }
 }
