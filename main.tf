fda1246ef380b251a418b9d102004919880a4ee6
Chikara <chtakahashi10@gmail.com>
2022-01-18 10:54:04 -0500

    add the ability to optionally encrypt cloudwatch logs and to manage the default security group of the atlantis vpc

--- main.tf
+++ main.tf
@@ -192,6 +192,10 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 
+  manage_default_security_group  = var.manage_default_security_group
+  default_security_group_ingress = var.default_security_group_ingress
+  default_security_group_egress  = var.default_security_group_egress
+
   tags = local.tags
 }
 
@@ -690,6 +694,7 @@ resource "aws_ecs_service" "atlantis" {
 resource "aws_cloudwatch_log_group" "atlantis" {
   name              = var.name
   retention_in_days = var.cloudwatch_log_retention_in_days
+  kms_key_id        = var.cloudwatch_logs_kms_key_id
 
   tags = local.tags
 }
