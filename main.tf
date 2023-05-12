375a926af8c5c9020e21861b45eb5cb9380cf776
chtakahashi <61299314+chtakahashi@users.noreply.github.com>
2023-05-12 11:02:29 -0400

    feat: Make the enable_nat_gateway and single_nat_gateway variables configurable (#343)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- main.tf
+++ main.tf
@@ -221,8 +221,8 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = var.enable_nat_gateway
+  single_nat_gateway   = var.single_nat_gateway
   enable_dns_hostnames = !var.enable_ephemeral_storage
 
   manage_default_security_group  = var.manage_default_security_group
