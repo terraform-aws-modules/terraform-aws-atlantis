5d965f547e571c50e6de9a6cd77a08011cb81b26
Bryant Biggs <bryantbiggs@gmail.com>
2023-11-30 07:47:08 -0500

    chore: Update formatting

--- main.tf
+++ main.tf
@@ -485,7 +485,10 @@ module "efs" {
   override_policy_documents          = try(var.efs.override_policy_documents, [])
   policy_statements = concat(
     [{
-      actions = ["elasticfilesystem:ClientMount", "elasticfilesystem:ClientWrite"]
+      actions = [
+        "elasticfilesystem:ClientMount",
+        "elasticfilesystem:ClientWrite",
+      ]
       principals = [
         {
           type        = "AWS"
